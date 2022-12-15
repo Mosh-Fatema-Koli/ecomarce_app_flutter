4c32f77a26f504ea66314b4335aebe9857045be8
Mosh-Fatema-Koli <fatemakoli58@gnail.com>
2022-12-15 17:24:20 +0600

    banner added

--- lib/all Screen/home.dart
+++ lib/all Screen/home.dart
@@ -1,4 +1,5 @@
 
+import 'package:banner_carousel/banner_carousel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,8 +10,44 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
 
-      body: Text("hi"),
+      body: Column(
+       
+        children: <Widget>[
+     
+            /// Carousel FullScreen
+          BannerCarousel.fullScreen(
+            banners: BannerImages.listBanners,
+            height: 200,
+            animation: false,
+            initialPage: 1,
+            // OR pageController: PageController(initialPage: 6),
+          ),
 
+       
+          SizedBox(
+            height: 20,
+          ),
+
+
+        ],
+      ),
     );
   }
+}
+
+class BannerImages {
+  static const String banner1 =
+      "https://play-lh.googleusercontent.com/kvaPFPHuLufB1rbLehs9LukdubfLfD02Encv16k55Kth-zthVtBrT77mLoobSBPpGw=w526-h296-rw";
+  static const String banner2 =
+      "https://i.pinimg.com/474x/bb/b2/26/bbb226401ef104c0abc1ab0b9209b370.jpg";
+  static const String banner3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdk1urD8pPjSSLAWZPec35ahY77JRTnsluONyUMnxAzK0dpilrkTnL2wqZEajs7bHXpUU&usqp=CAU";
+  static const String banner4 =
+      "https://img.freepik.com/free-vector/hand-drawn-vegetables-supermarket-twitter-header_23-2149385522.jpg?auto=format&h=200";
+
+  static List<BannerModel> listBanners = [
+    BannerModel(imagePath: banner1, id: "1"),
+    BannerModel(imagePath: banner2, id: "2"),
+    BannerModel(imagePath: banner3, id: "3"),
+    BannerModel(imagePath: banner4, id: "4"),
+  ];
 }
